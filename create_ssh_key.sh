bb2a82eebbda65075dab0f9adb1f3c557368bca4
Jonathan Hannay <31388553+swfc@users.noreply.github.com>
2023-03-06 21:44:17 -0500

    Initial commit.

---
+++ create_ssh_key.sh
@@ -0,0 +1,16 @@
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "This script must be sourced, not run as a standalone script."
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <ssh-key-filename>"
+  exit 1
+fi
+
+filename="$HOME/.ssh/$1"
+ssh-keygen -t ed25519 -C "hannayjd@$HOSTNAME" -f "$filename"
+eval "$(ssh-agent -s)"
+ssh-add $filename
+ssh-add -L
+
