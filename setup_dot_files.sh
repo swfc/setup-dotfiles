d9fd2d2f711826e894de935ebcafe760b3589da6
swfc <31388553+swfc@users.noreply.github.com>
2023-05-30 18:29:22 -0400

    Clone oh-my-zsh items instead of submodules.

--- setup_dot_files.sh
+++ setup_dot_files.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 cd $HOME
+# install oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install custom plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install powerlevel10k custom theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# initialize and clone dotfile into home dir
 git init
 git remote add origin git@github.com:swfc/dotfiles.git
 git fetch
 git checkout -f master
-git submodule update --init --recursive
-
