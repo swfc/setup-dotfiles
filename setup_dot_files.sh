5096f5f44c3fe71c484ca9350da60c142a44777b
Jonathan Hannay <31388553+swfc@users.noreply.github.com>
2023-06-30 17:11:37 -0400

    Fixes to allow choosing install dir.

--- setup_dot_files.sh
+++ setup_dot_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 
-cd $HOME
+DIR=$HOME
+cd $DIR
 
 # check if zsh is installed
 if command -v zsh >/dev/null 2>&1; then
@@ -10,16 +12,19 @@ else
     sudo apt update -y
     sudo apt install -y zsh
 fi
-
+# set ZSH
+ZSH=$DIR/.oh-my-zsh
 # install oh-my-zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+chmod +x install.sh
+./install.sh --skip-chsh --unattended
 
 # install custom plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$DIR/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$DIR/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # install powerlevel10k custom theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$DIR/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # initialize and clone dotfile into home dir
 git init
